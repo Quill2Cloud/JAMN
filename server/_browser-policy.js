323a50170bdf623f95d3ef52bb94855577ad77c1
Michael Grahame <mgrahame@gmail.com>
2015-05-13 22:17:04 -0600

    Fixed browser-policy: moved to server, added origins

---
+++ server/_browser-policy.js
@@ -0,0 +1,6 @@
+BrowserPolicy.content.allowOriginForAll("*.cloudfront.net");
+BrowserPolicy.content.allowOriginForAll("*.heapanalytics.com");
+BrowserPolicy.content.allowOriginForAll("cdn.inspectlet.com");
+BrowserPolicy.content.allowOriginForAll("hn.inspectlet.com");
+BrowserPolicy.content.allowOriginForAll("cdn.mouseflow.com");
+BrowserPolicy.content.allowOriginForAll("www.navilytics.com");
