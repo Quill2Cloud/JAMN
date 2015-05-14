c5dddc14e3efd7fc2894036029abe371d6325833
Michael Grahame <mgrahame@gmail.com>
2015-05-13 22:55:09 -0600

    Updated browser-policy: added origins and modified permissions

--- server/_browser-policy.js
+++ server/_browser-policy.js
@@ -1,6 +1,22 @@
-BrowserPolicy.content.allowOriginForAll("*.cloudfront.net");
-BrowserPolicy.content.allowOriginForAll("*.heapanalytics.com");
-BrowserPolicy.content.allowOriginForAll("cdn.inspectlet.com");
-BrowserPolicy.content.allowOriginForAll("hn.inspectlet.com");
-BrowserPolicy.content.allowOriginForAll("cdn.mouseflow.com");
-BrowserPolicy.content.allowOriginForAll("www.navilytics.com");
+BrowserPolicy.framing.disallow();
+BrowserPolicy.content.disallowInlineScripts();
+BrowserPolicy.content.disallowEval();
+BrowserPolicy.content.allowInlineStyles();
+BrowserPolicy.content.allowFontDataUrl();
+
+var trusted = [
+  'http://www.google-analytics.com',
+  'https://d37gvrvc0wt4s1.cloudfront.net', //rollbar
+  'http://d37gvrvc0wt4s1.cloudfront.net', //rollbar
+  'http://d1l6p2sc9645hc.cloudfront.net',
+  'http://d36lvucg9kzous.cloudfront.net',
+  'http://cdn.inspectlet.com',
+  'http://hn.inspectlet.com',
+  'http://heapanalytics.com',
+  'http://cdn.mouseflow.com',
+  'http://www.navilytics.com'
+];
+
+_.each(trusted, function(origin) {
+  BrowserPolicy.content.allowOriginForAll(origin);
+});
