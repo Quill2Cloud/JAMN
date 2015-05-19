03eb5c9de31e2be8777e4f7ddd066e0b35674917
Michael Grahame <mgrahame@gmail.com>
2015-05-18 22:08:08 -0600

    Removed extra analytics sites

--- server/_browser-policy.js
+++ server/_browser-policy.js
@@ -6,15 +6,6 @@ BrowserPolicy.content.allowFontDataUrl();
 
 var trusted = [
   'http://www.google-analytics.com',
-  'https://d37gvrvc0wt4s1.cloudfront.net', //rollbar
-  'http://d37gvrvc0wt4s1.cloudfront.net', //rollbar
-  'http://d1l6p2sc9645hc.cloudfront.net',
-  'http://d36lvucg9kzous.cloudfront.net',
-  'http://cdn.inspectlet.com',
-  'http://hn.inspectlet.com',
-  'http://heapanalytics.com',
-  'http://cdn.mouseflow.com',
-  'https://n2.mouseflow.com',
   'http://www.navilytics.com'
 ];
 
